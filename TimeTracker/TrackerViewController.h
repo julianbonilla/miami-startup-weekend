85b2d3a2b56a020f572384b67cad1180160d455c
Julian Bonilla <julianjbonilla@gmail.com>
2011-05-21 16:40:19 -0400

    Add nav controller and tracker view.

---
+++ TimeTracker/TrackerViewController.h
@@ -0,0 +1,16 @@
+//
+//  TrackerViewController.h
+//  TimeTracker
+//
+//  Created by Julian Bonilla on 5/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TrackerViewController : UIViewController {
+    
+}
+
+@end
